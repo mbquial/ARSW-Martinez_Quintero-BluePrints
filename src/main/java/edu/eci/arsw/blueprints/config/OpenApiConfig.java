f0b44269987fa3557648a3bbb21846626f85af01
mbquial <pepitotolitis@gmail.com>
2026-02-20 08:41:04 -0500

    refactor: remove unused import for Contact in OpenApiConfig

--- src/main/java/edu/eci/arsw/blueprints/config/OpenApiConfig.java
+++ src/main/java/edu/eci/arsw/blueprints/config/OpenApiConfig.java
@@ -1,7 +1,6 @@
 package edu.eci.arsw.blueprints.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
